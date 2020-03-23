826180b3d4a6011a839d6d2caf5b73fea1a298b0
Vinodkumar SC <scvinodkumar.php@gmail.com>
2020-03-23 13:23:25 +0530

    Added files related to site api

---
+++ src/Routing/CustomRouteSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\site_api_key\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class CustomRouteSubscriber extends RouteSubscriberBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function alterRoutes(RouteCollection $collection)
+    {
+        if($route = $collection->get('system.site_information_settings'))
+        {
+            $route->setDefault('_form', 'Drupal\site_api_key\Form\CustomSiteInformationForm');
+        }
+    }
+}
